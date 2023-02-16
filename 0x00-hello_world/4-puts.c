8d76568ce3442df04304c2a9e97ad3ab6af22198
joinforbrown <callme508charles@gmail.com>
2023-02-16 09:10:24 -0800

    Task 4 Hello Put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * 
- * Description: 'the program's description'
- * 
- * Return: Always 0 (Success)
+ *main: 'Entry point' 
+ *Description: 'the program's description'
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
