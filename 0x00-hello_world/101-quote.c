9c222bff44f2befbdd85c389dde25d9b1e30099b
joinforbrown <callme508charles@gmail.com>
2023-02-16 16:08:54 -0800

    Task 8  UNIX is basically a simple operating system but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ * Return: 1(success)
+ */
+
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
