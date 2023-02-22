26292a4dde0b800b3572358f43708006399461ec
joinforbrown <callme508charles@gmail.com>
2023-02-22 03:25:38 -0800

    Task 9 Learn your times table

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - Write a function that prints the 9 times table, starting with 0.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    times_table();
-    return (0);
+	times_table();
+	return (0);
 }
