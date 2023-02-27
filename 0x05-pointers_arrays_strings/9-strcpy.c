ee02dda711fca647bd6256ea6573f859e5b3cdde
joinforbrown <callme508charles@gmail.com>
2023-02-27 11:04:28 -0800

    Task 9 Strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,18 +5,16 @@
  *           terminating null byte, to a buffer pointed to by @dest.
  * @dest: A buffer to copy the string to.
  * @src: The source string to copy.
- *
  * Return: A pointer to the destination string @dest.
  */
+
 char *_strcpy(char *dest, const char *src)
 {
-	int index = 0;
-
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
-
-	return (dest);
+int i;
+for (i = 0; src[i] !='\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[i++] = '\0';
+return (dest);
 }
