702bc8837d7db5e2494c1ce1f7a51e6e05b91e7e
joinforbrown <callme508charles@gmail.com>
2023-02-27 08:42:43 -0800

    Task 6 Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints one char out of two of a string.
+ * @str: The string containing characters.
+ */
+void puts2(char *str)
+{
+	int 1; 
+	j = 0;
+
+	while (str[j] != '\0')
+	{
+	j++;
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}
