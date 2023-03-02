47c2c635b23c405add9baa10dcfc20f4ea1a2a07
joinforbrown <callme508charles@gmail.com>
2023-03-02 07:20:02 -0800

    Task 1 Write a function that concatenates two strings

--- 0x06-pointers_arrays_strings/1-main.c
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -3,6 +3,7 @@
 
 /**
  * main - check the code
+ *
  * Return: Always 0.
  */
 int main(void)
@@ -12,7 +13,11 @@ int main(void)
     char *ptr;
     printf("%s\n", s1);
     printf("%s", s2);
-    ptr = _strcat(s1, s2);
+    ptr = _strncat(s1, s2, 1);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    printf("%s\n", ptr);
+    ptr = _strncat(s1, s2, 1024);
     printf("%s", s1);
     printf("%s", s2);
     printf("%s", ptr);
