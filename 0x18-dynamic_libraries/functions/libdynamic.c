36b2205e4c3a044174ecc853887babf42b22eeeb
joinforbrown <callme508charles@gmail.com>
2024-01-16 05:53:53 -0800

    Updated task 1&2

---
+++ 0x18-dynamic_libraries/functions/libdynamic.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+int _putchar(char c) {
+    // Implement _putchar function
+}
+
+int _islower(int c) {
+    // Implement _islower function
+}
+
+// Implement the rest of the functions similarly
+
+
